c3865c5659e28ebc8446fb560f40f80c8dcbe140
rikfor <rikard.forlin@gmail.com>
2023-12-19 13:13:38 +0100

    updated all figures - scRNAseq until Fig5 and extended

---
+++ figure_scripts/ExtendedFigures/ExtFig4ABCD.R
@@ -0,0 +1,99 @@
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(ggpubr)
+library(rstatix)
+
+setwd("Please insert working directory here to point at directory '../data/Figure4/'")
+
+info <- read_csv('frequency table/info.csv')
+
+# 4a
+
+data_freq <- read_csv('frequency table/CyTOF flowSOM level1.csv')
+
+d <- data_freq %>% 
+  select(-n) %>% 
+  tidyr::pivot_wider(names_from = flowSOM_level1, values_from = freq, values_fill = 0) %>%
+  mutate(CD4CD8_ratio = CD4Tcells/CD8Tcells) %>%
+  tidyr::pivot_longer(cols = !Subject, names_to = 'flowSOM_level1', values_to = 'freq') %>%
+  left_join(info, by='Subject')
+
+tb <- d %>% filter(flowSOM_level1 == 'CD4CD8_ratio') %>% mutate(timepoint_group = case_when(
+  Visit == 1 ~ 'baseline',
+  Visit == 2 ~ '3 mo',
+  Visit == 3 ~ '12 mo'
+)) %>% ungroup() %>% pairwise_t_test(freq ~ timepoint_group, paired = T)
+
+ggplot(d %>% filter(flowSOM_level1 == 'CD4CD8_ratio'), aes(x=timepoint, y=freq, group=SubjectID)) + 
+  geom_point() + geom_path() +
+  ylab('CD4T to CD8T ratio') + 
+  theme_bw() +
+  scale_x_continuous(breaks=c(0,3, 6, 9, 12))
+
+# 4bcg
+data_freq <- read_csv('frequency table/CyTOF flowSOM level2.csv')
+
+d <- data_freq %>% filter(flowSOM_level1 == 'CD4Tcells') %>%
+  mutate(custom_subpop = case_when(
+    flowSOM_level2 == 'CD4Tcells_Naive' ~ 'Naive CD4',
+    flowSOM_level2 %in% c('CD4Tcells_EM', 'CD4Tcells_TCM', 'CD4Tcells_TCM_CD127', 'CD4Tcells_TEMRA') ~ 'Memory CD4',
+    flowSOM_level2 == 'CD4Tcells_Treg' ~ 'Treg'
+  )) %>% 
+  group_by(Subject, custom_subpop) %>% 
+  summarise(freq = sum(freq)) %>%
+  ungroup() %>% 
+  tidyr::complete(Subject, custom_subpop, fill = list(freq=0)) %>%
+  group_by(Subject) %>%
+  mutate(freq_of_prev = freq/sum(freq)) %>%
+  left_join(info, by='Subject')
+
+tb <- d %>% mutate(timepoint_group = case_when(
+  Visit == 1 ~ 'baseline',
+  Visit == 2 ~ '3 mo',
+  Visit == 3 ~ '12 mo'
+)) %>% 
+  group_by(custom_subpop) %>% pairwise_t_test(freq ~ timepoint_group, paired = T)
+
+# 4b
+ggplot(d %>% filter(custom_subpop == 'Naive CD4'), aes(x=timepoint, y=freq, group=SubjectID)) +
+  geom_point() + geom_path() + 
+  theme_bw() +
+  scale_x_continuous(breaks=c(0,3, 6, 9, 12)) +
+  ggtitle('Naive CD4')
+
+# 4f
+ggplot(d %>% filter(custom_subpop == 'Treg'), aes(x=timepoint, y=freq, group=SubjectID)) +
+  geom_point() + geom_path() + 
+  theme_bw() +
+  scale_x_continuous(breaks=c(0,3, 6, 9, 12)) +
+  ggtitle('Treg')
+
+# 4c
+d <- data_freq %>% filter(flowSOM_level1 == 'CD8Tcells') %>%
+  mutate(custom_subpop = case_when(
+    flowSOM_level2 %in% c('CD8Tcells_Naive', 'CD8Tcells_Naive_CD24') ~ 'Naive CD8',
+    flowSOM_level2 %in% c('CD8Tcells_EM', 'CD8Tcells_TCM', 'CD8Tcells_TCM_CD24', 'CD8Tcells_TEMRA', 'CD8Tcells_TEMRA_CD24', 'CD8Tcells_TEMRA_CD57') ~ 'Memory CD8',
+    flowSOM_level2 == 'CD8Tcells_MAIT' ~ 'MAIT CD8'
+  )) %>% 
+  group_by(Subject, custom_subpop) %>% 
+  summarise(freq = sum(freq)) %>%
+  ungroup() %>% 
+  tidyr::complete(Subject, custom_subpop, fill = list(freq=0)) %>%
+  group_by(Subject) %>%
+  mutate(freq_of_prev = freq/sum(freq)) %>%
+  left_join(info, by='Subject')
+
+tb <- d %>% mutate(timepoint_group = case_when(
+  Visit == 1 ~ 'baseline',
+  Visit == 2 ~ '3 mo',
+  Visit == 3 ~ '12 mo'
+)) %>% 
+  group_by(custom_subpop) %>% pairwise_t_test(freq ~ timepoint_group, paired = T)
+
+ggplot(d %>% filter(custom_subpop == 'Naive CD8'), aes(x=timepoint, y=freq, group=SubjectID)) +
+  geom_point() + geom_path() + 
+  theme_bw() +
+  scale_x_continuous(breaks=c(0,3, 6, 9, 12)) +
+  ggtitle('Naive CD8')
+
--- figure_scripts/Figure4/Figure4ANicheNet.R
+++ figure_scripts/Figure4/Figure4ANicheNet.R
@@ -1,105 +1,128 @@
-#Script to generate Figure4J in Lakshmikanth, Consiglio et al - Immune system adapaption to Gender affirming Testosterone Therapy
-#Author Rikard Forlin - rikard.forlin@ki.se
+#Script to generate Figure4A in Lakshmikanth, Consiglio et al - Immune system adaptation during gender affirming testosterone therapy
+#Author of script Rikard Forlin - rikard.forlin@ki.se
 library(nichenetr)
 library(tidyverse)
-library(SeuratData)
-library(SeuratDisk)
 library(Seurat)
 library(anndata)
 library(ggplot2)
 library(circlize)
 library(colormap)
+library(SeuratDisk)
 
+
+#### GENELISTS ####
+#Th1-response from https://www.gsea-msigdb.org/gsea/msigdb/human/geneset/BOSCO_TH1_CYTOTOXIC_MODULE.html
+th1_response <- c(
+  "ABCB1", "ADAM19", "ADAM3A", "ADORA3", "ANKRD22", "APOL3", "ARNT2", "ATF5", "BATF2", "BCL2",
+  "BCL2L14", "TMEM229B", "ANXA2R", "KNL1", "CCL7", "CCL8", "CCR5", "CD163L1", "CD28", "CD300E",
+  "CD38", "CD5", "CDC7", "CDK6", "CH25H", "CMKLR1", "CMPK2", "CRABP1", "CTLA4", "CXCL10", "CXCL11",
+  "CXCL9", "DNA2", "DPCD", "ENPP2", "F13A1", "F2R", "FAM20A", "CALHM6", "STRIP2", "FBXO39", "FCGR2B",
+  "FFAR3", "FUT2", "GAS6", "GBP6", "GIMAP4", "GIMAP5", "GIMAP7", "GIMAP8", "GNLY", "GPR171", "GZMB",
+  "GZMK", "HAMP", "HAPLN3", "HERC6", "HESX1", "HS3ST3B1", "IFNB1", "IFNG", "IL10", "IL12RB2", "IL15",
+  "IL15RA", "IL21", "IL2RA", "IL4I1", "IDO1", "IRF4", "ACOD1", "ISG15", "ITGA9", "CEMIP", "KLHDC1",
+  "KLRD1", "CERS4", "LILRB5", "MERTK", "MKI67", "MS4A6E", "MYBL1", "NKG7", "OR2A5", "OR5D14", "OR6K6",
+  "P2RX5", "PLEKHO1", "PRF1", "PRKCA", "PRKCQ", "PTGER2", "RGL1", "RHBDF2", "RNASE2", "RRM2", "RTP4",
+  "RUNX3", "SAMD3", "SDS", "SH2D1A", "SLA2", "SLAMF1", "SMOX", "SOCS1", "SSTR2", "STAB1", "STAT4",
+  "TNFSF18", "TRAT1", "TSHZ3", "TXK", "TYMS", "USP18"
+)
+
+
+th1_drivers <- c('TNF','IL15', 'IL6', 'IL12B') #IL12A too sparsely expressed
+
+######## Run NicheNet-analysis ########
 options(warn=-1)
 # First load the ligand-target matrices from NicheNet and the weighted networks.
 ligand_target_matrix = readRDS(url("https://zenodo.org/record/3260758/files/ligand_target_matrix.rds"))
-#ligand_target_matrix[1:5,1:5]
+
 lr_network = readRDS(url("https://zenodo.org/record/3260758/files/lr_network.rds"))
 weighted_networks = readRDS(url("https://zenodo.org/record/3260758/files/weighted_networks.rds"))
 weighted_networks_lr = weighted_networks$lr_sig %>% inner_join(lr_network %>% distinct(from,to), by = c("from","to"))
 
+#Read in adata made for NicheNet analysis (found in jupyter script for Figure4_scRNA-seq, "preparing for NicheNet")
+count_data_str <- 'insert pathway generated from jupyter script'
+meta_data_str <- 'insert pathway generated from jupyter script'
 
-#Read in adata made for NicheNet analysis (found in jupyter script, "Prepare for NicheNet")
-# and make it into a seurat object
-adata <- read_h5ad("nichenet_adata.h5ad")#Insert pathway to annotated dataframe here
-rownames(adata$obs) <- make.names(rownames(adata$obs), unique = TRUE)
+expression_data <- read.csv(count_data_str, row.names = 1)
+expression_data <- t(expression_data)
 
-X <- as.matrix(adata$X)
-seuratObj <- CreateSeuratObject(counts = t(X), meta.data = adata$obs)
-seuratObj@meta.data$celltype = adata$obs$celltype
-seuratObj@meta.data$celltype_sub = adata$obs$celltype_sub
-seuratObj@meta.data$V = adata$obs$V
+metadata <- read.csv(meta_data_str, row.names = 1)
+
+seuratObj <- CreateSeuratObject(counts = expression_data)
+seuratObj@meta.data <- metadata
 
 
-#Remove cell types that we are making up a small percentage of the population, X is unknown cell types
-seuratObj@meta.data$celltype = as.character(seuratObj@meta.data$celltype)
-inds = rownames(seuratObj@meta.data[seuratObj@meta.data$celltype %in% c('MAIT', 'Megakaryocyte', 'gDT', 'X'), ])
-seuratObj <- seuratObj[, ! rownames(seuratObj@meta.data) %in% inds]
+#If you want to convert into SeuratObject to not read the .csv files every time:
+#Convert("/Users/rikardforlin/Forskning/Data/GenderAffirmingTestosteroneTreatment/NicheNet-data/LPS/ForNicheNetLPS_GeneCountOver3kNoLayers.h5ad", dest = "h5seurat", overwrite = FALSE)
+#seuratObj <- LoadH5Seurat("/Users/rikardforlin/Forskning/Data/GenderAffirmingTestosteroneTreatment/NicheNet-data/LPS/ForNicheNetLPS_GeneCountOver3kNoLayers.h5seurat", meta.data = FALSE, misc = FALSE, assays = "RNA")
+#seuratObj@meta.data <- metadata
 
-#Removal of Tregs
-inds = rownames(seuratObj@meta.data[seuratObj@meta.data$celltype_sub %in% c('Treg'), ])
-seuratObj <- seuratObj[, ! rownames(seuratObj@meta.data) %in% inds]
+#Normalize the data
+seuratObj <- NormalizeData(seuratObj)
 
-unique(seuratObj@meta.data$celltype_sub)
+#####################################################################
 
+all_cells <- c('CD8T', 'NK', 'Monocyte')
+inds = rownames(seuratObj@meta.data[seuratObj@meta.data$CellType %in% all_cells, ])
+seuratObj <- seuratObj[, rownames(seuratObj@meta.data) %in% inds]
 #Set identity to cell type
-Idents(object = seuratObj) <- seuratObj@meta.data$celltype
+Idents(object = seuratObj) <- seuratObj@meta.data$CellType
 
-#Set identity to cell type
-Idents(object = seuratObj) <- seuratObj@meta.data$celltype
+# List of all cell types
+#all_receiver <- c('CD8T', 'CD4T', 'NK')
+all_receiver <- c('CD8T', 'NK')
 
-#This is a bit messy and I'm sorry for that - my R skills are under improvement...
-#But here we get expressed genes from each cell type, and look into the ligand_target_matrix from NicheNet to define a background gene set
-#Only genes that is a target for a ligand is included in the background_expressed_genes for downstream analysis
-receiver = 'Monocyte'
-expressed_genes_receiver = get_expressed_genes(receiver, seuratObj, pct = 0.10)
-receiver2 = 'CD8T'
-expressed_genes_receiver2 = get_expressed_genes(receiver2, seuratObj, pct = 0.10)
-receiver3 = 'CD4T'
-expressed_genes_receiver3 = get_expressed_genes(receiver3, seuratObj, pct = 0.10)
-receiver4 = 'NK'
-expressed_genes_receiver4 = get_expressed_genes(receiver4, seuratObj, pct = 0.10)
-receiver5 = 'pDC'
-expressed_genes_receiver5 = get_expressed_genes(receiver5, seuratObj, pct = 0.10)
-receiver6 = 'DC'
-expressed_genes_receiver6= get_expressed_genes(receiver6, seuratObj, pct = 0.10)
-receiver7 = 'B'
-expressed_genes_receiver7= get_expressed_genes(receiver7, seuratObj, pct = 0.10)
-all_receiver = c('CD8T', 'CD4T', 'NK', 'pDC','DC','B', 'Monocyte')
+# Get all all expressed genes into a single vector
+all_expressed_genes_receiver <- rownames(seuratObj@assays$RNA$data)
 
-expressed_genes_receiver = c(expressed_genes_receiver, expressed_genes_receiver2,expressed_genes_receiver3,expressed_genes_receiver4, expressed_genes_receiver5, expressed_genes_receiver6, expressed_genes_receiver7)
-expressed_genes_receiver = unique(expressed_genes_receiver)
-background_expressed_genes = expressed_genes_receiver %>% .[. %in% rownames(ligand_target_matrix)]
+############################################################################################
+# target genes in rows, ligands in columns
+background_expressed_genes = all_expressed_genes_receiver %>% .[. %in% rownames(ligand_target_matrix)]
 
 #Define what cell type is the sender - and look for expressed genes for that cell type
-#This is the same as expressed_genes_receiver, but for clarification I left it here
-ct_sender = 'CD4T'
+ct_sender = 'Monocyte'
 sender_celltypes = ct_sender
-expressed_genes_sender = get_expressed_genes(sender_celltypes, seuratObj, pct = 0.10)
-expressed_genes_sender = unique(expressed_genes_sender)
+
+# target genes in rows, ligands in columns
+expressed_genes_sender <- all_expressed_genes_receiver %>% .[. %in% colnames(ligand_target_matrix)]
+
+seurat_obj_receiver= subset(seuratObj, idents = all_receiver)
+
+Idents(seurat_obj_receiver) <- "stimulation"
+condition_oi = "lps-V2"
+condition_reference = "lps-V1" 
 
 #Define a gene set of interest: here we take differentially expressed genes in pre and post testosterone-treatment
-seurat_obj_receiver= subset(seuratObj, idents = all_receiver)
-seurat_obj_receiver = SetIdent(seurat_obj_receiver, value = seurat_obj_receiver[["V"]])
-condition_oi = "V2"
-condition_reference = "V1" 
-
-DE_table_receiver = FindMarkers(object = seurat_obj_receiver, ident.1 = condition_oi, ident.2 = condition_reference, min.pct = 0.10) %>% rownames_to_column("gene")
+DE_table_receiver = FindMarkers(object = seurat_obj_receiver, ident.1 = condition_oi, ident.2 = condition_reference, min.pct = 0.1,logfc.threshold = 0.1) %>% rownames_to_column("gene") #change_here?
 DE_table_receiver <- DE_table_receiver %>% drop_na()
+DE_table_receiver <- DE_table_receiver[DE_table_receiver$gene %in% th1_response, ]
 
 #Filter the DEG-geneset
-geneset_oi = DE_table_receiver %>% filter(p_val_adj <= 0.05 & abs(avg_log2FC) >= 0.25) %>% pull(gene)
+geneset_oi = DE_table_receiver %>% filter(p_val_adj <= 0.05 & avg_log2FC >= 0.15) %>% pull(gene)
 geneset_oi = geneset_oi %>% .[. %in% rownames(ligand_target_matrix)]
+
+#Define gene set of interest for the sender cells (Monocytes)
+
+seurat_obj_sender = subset(seuratObj, idents = ct_sender)
+#seurat_obj_sender  = SetIdent(seurat_obj_sender , value = seurat_obj_sender [["stimulation"]])
+Idents(seurat_obj_sender) <- "stimulation"
+
+DE_table_sender = FindMarkers(object = seurat_obj_sender , ident.1 = condition_oi, ident.2 = condition_reference, min.pct = 0.1, logfc.threshold = 0.1) %>% rownames_to_column("gene") # logfc.threshold = 0.25
+DE_table_sender <- DE_table_sender %>% drop_na()
+DE_table_sender <- DE_table_sender[DE_table_sender$gene %in% th1_drivers, ]
+
+geneset_oi_ligands = DE_table_sender %>% pull(gene)
+geneset_oi_ligands = geneset_oi_ligands %>% .[. %in% colnames(ligand_target_matrix)]
+
 
 #Define a set of potential ligands expressed by the sender populations and bind a receptor expressed on the receiver/target population
 ligands = lr_network %>% pull(from) %>% unique()
 receptors = lr_network %>% pull(to) %>% unique()
 
-expressed_ligands = intersect(ligands,expressed_genes_sender)
-expressed_receptors = intersect(receptors,expressed_genes_receiver)
+expressed_ligands = intersect(ligands,geneset_oi_ligands)
+expressed_receptors = intersect(receptors,all_expressed_genes_receiver)
 
 potential_ligands = lr_network %>% filter(from %in% expressed_ligands & to %in% expressed_receptors) %>% pull(from) %>% unique()
-#head(potential_ligands)
+
 
 #Perform NicheNet ligand activity analysis: rank the potential ligands based on the presence of their target genes in the gene set of intereset (compared to the background set of genes)
 ligand_activities = predict_ligand_activities(geneset = geneset_oi, background_expressed_genes = background_expressed_genes, ligand_target_matrix = ligand_target_matrix, potential_ligands = potential_ligands)
@@ -107,32 +130,31 @@ ligand_activities = ligand_activities %>% arrange(-pearson) %>% mutate(rank = ra
 
 # Auroc, Aupr, pearson are measure for how well a ligand can predict the observed DEGs compared to the background set of expressed genes
 # In their validation study, they showed that pearson correlation coefficient between a ligands target predictions and the observed transcriptional response was the most informative measure to define ligand activity.
-#ligand_activities[1:10,]
-#Take the top 15 of pearson correlation coefficient
-best_upstream_ligands = ligand_activities %>% top_n(15, pearson) %>% arrange(-pearson) %>% pull(test_ligand) %>% unique()
+#Take the top 100 of pearson correlation coefficient - this is very high but we'll filter later on
+best_upstream_ligands = ligand_activities %>% top_n(100, pearson) %>% arrange(-pearson) %>% pull(test_ligand) %>% unique()
 
 ########################################################################################
 ##Circos plot visualization to show active ligand-target links between interacting cells
 ##https://github.com/saeyslab/nichenetr/blob/2cb19de12d7ff8d3d8491302a43e5b3a6d3c0ae9/vignettes/circos.md
 
-#We only show ligands with a weight higher than a predefined cutoff: links belonging to the 63% of lowest scores were removed. 
+#We only show ligands with a weight higher than a predefined cutoff: links belonging to the 66% of lowest scores were removed. 
 #Find and set cutoff for the highest weighted ligands.
 active_ligand_target_links_df = best_upstream_ligands %>% lapply(get_weighted_ligand_target_links,geneset = geneset_oi, ligand_target_matrix = ligand_target_matrix, n = 20000) %>% bind_rows() %>% drop_na()
 active_ligand_target_links_df <- active_ligand_target_links_df[order(active_ligand_target_links_df$weight, decreasing = TRUE), ]
+
+
 active_ligand_target_links_df <- head(active_ligand_target_links_df, 100)
 cutoff_include_all_ligands = active_ligand_target_links_df$weight %>% quantile(0.66)
 
-active_ligand_target_links_df
+active_ligand_target_links_df_circos = active_ligand_target_links_df %>% filter(weight > cutoff_include_all_ligands)
 
 #Filter ligands with the cutoff
-active_ligand_target_links_df_circos = active_ligand_target_links_df %>% filter(weight > cutoff_include_all_ligands)
 ligands_to_remove = setdiff(active_ligand_target_links_df$ligand %>% unique(), active_ligand_target_links_df_circos$ligand %>% unique())
 targets_to_remove = setdiff(active_ligand_target_links_df$target %>% unique(), active_ligand_target_links_df_circos$target %>% unique())
 circos_links = active_ligand_target_links_df %>% filter(!target %in% targets_to_remove &!ligand %in% ligands_to_remove)
 
 circos_links$ligand_type = ct_sender
 circos_links$target_type = 'all'
-
 
 #Prepare the circos visualization: give each segment of ligands and targets a specific color and order
 grid_col_tbl_ligand = tibble(ligand_type = ct_sender)
@@ -150,21 +172,7 @@ grid_ligand_color = ligand_color$color_ligand_type %>% set_names(ligand_color$li
 ###Create specific colors according to which celltype has the highest mean gene count of target gene
 target_color = circos_links %>% distinct(target)
 
-#ccolors <- colormap(colormap = colormaps$YIOrRd, nshades = length(target_color[[1]]))
-#target_color$color_target_type <- ccolors
-#monocyte = purple
-#NK = #FF3300 (red)
-#pDC = #3333FF (blue)
-#CD4T = #339966 (green)
-#DC = orange
-#CD8T = yellow
-#B = black
-
-target_color$color_target_type <- c("purple", "purple", "purple", "#FF3300", "#3333FF", "#FF3300", "purple", "#339966", "purple","purple", 
-                                    "#FF3300", "#339966", "#3333FF", "#339966", "#FF3300", "orange", "orange", 
-                                    "#339966", "yellow", "purple","orange", "purple","#339966", "#339966", "purple","black", "#FF3300", "purple")
-
-#target_color$color_target_type = colormap(colormap = colormaps$viridis, length(target_color[[1]]))
+target_color$color_target_type = colormap(colormap = colormaps$viridis, length(target_color[[1]]))
 
 #### Inserted color according to mean expression of gene (calculated in python)
 grid_target_color = target_color$color_target_type %>% set_names(target_color$target)
@@ -188,7 +196,6 @@ gaps = c(
   rep(width_same_cell_same_target_type, times = (circos_links %>% filter(target_type == "all") %>% distinct(target) %>% nrow() -1)),
   width_ligand_target
 )
-unique(links_circle$target)
 
 
 circos.clear()
@@ -201,7 +208,4 @@ circos.track(track.index = 1, panel.fun = function(x, y) {
               facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.55), cex = 1)
 }, bg.border = NA) #
 
-
-
-
-
+unique(circos_links$target)
